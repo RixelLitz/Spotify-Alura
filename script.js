5f4fd1431dd239cf424c64e838bafca01f767da0
Rian Britto Pereira <contatorianbritto@gmail.com>
2024-01-26 18:50:01 -0300

    Colocando Fake API no ar

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const resultsArtist = document.getElementById("result-artist")
 const resultPlaylist = document.getElementById("cards-container")
 
 function requestApi(searchTerm) {
-  const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+  const url = `https://json-server-nine-xi.vercel.app/artists?name_like=${searchTerm}`
   fetch(url)
     .then((response) => response.json())
     .then((result) => displayResults(result))
