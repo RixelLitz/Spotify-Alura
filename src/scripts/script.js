1dd5204e72ec1ce3e9ef6af33b7e574dfbb991d3
Rian Britto Pereira <contatorianbritto@gmail.com>
2024-01-30 19:11:01 -0300

    Responsividade ajustada + Sistema de saudação com data e hora implementado

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -32,3 +32,54 @@ document.addEventListener("input", function () {
 
   requestApi(searchTerm)
 })
+document.addEventListener("DOMContentLoaded", function () {
+  saudacaoDinamica()
+})
+
+function saudacaoDinamica() {
+  var saudacaoElemento = document.getElementById("greetings")
+  var dataAtual = new Date()
+  var horaAtual = dataAtual.getHours()
+
+  var saudacao
+
+  if (horaAtual >= 6 && horaAtual < 12) {
+    saudacao = "Bom dia"
+  } else if (horaAtual >= 12 && horaAtual < 18) {
+    saudacao = "Boa tarde"
+  } else {
+    saudacao = "Boa noite"
+  }
+
+  saudacaoElemento.textContent = saudacao
+}
+document.addEventListener("DOMContentLoaded", function () {
+  exibirDataHorario()
+  setInterval(exibirDataHorario, 1000)
+})
+
+function exibirDataHorario() {
+  var dataElemento = document.getElementById("date")
+  var horarioElemento = document.getElementById("hour")
+
+  var dataAtual = new Date()
+
+  var optionsData = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+  var optionsHorario = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false,
+  }
+
+  var dataFormatada = dataAtual.toLocaleDateString("pt-BR", optionsData)
+  var horarioFormatado = dataAtual.toLocaleTimeString("pt-BR", optionsHorario)
+
+  dataElemento.textContent = dataFormatada
+  horarioElemento.textContent = horarioFormatado
+}
